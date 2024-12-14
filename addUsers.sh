1f8afefa0eb24c153f17fb618f39b64d5d76bfc1
AdeL448 <adel.no@outlook.com>
2024-12-14 11:43:59 +0100

    addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 if [ -z $@  ]; then
-    echo "Merci de renseigner un nom d'utilisateur pour la création !"
+    echo "Merci de renseigner un nom d'utilisateur pour la création..."
     exit 1
 fi
 
 for utilisateur in "$@"; do
    
-    if [ "$utilisateur" &>/dev/null ]; then
-        echo "le nom d'utilisateur "$utlisateur" existe déjà"
+    if [ -d $utilisateur ]; then
+        echo "le nom d'utilisateur "$utlisateur" existe déjà !"
         continue
     fi
-       
       sudo useradd "$utilisateur"
    
     if [ $? -eq 0 ]; then
-        echo "L'utilisateur $utilsateur a été créé avec succès !"
+        echo "L'utilisateur $utilisateur a été créé avec succès !"
     else
-        echo "La création de l'utilisateur $utilsateur a échoué..."
+        echo "La création de l'utilisateur $utilisateur a échoué..."
     fi
 done
